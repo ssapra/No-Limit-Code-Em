51690a3eb0cdd236f3f578a0c6a3c36b3dd6e24c
Shaan Sapra <ssapra@uchicago.edu>
2012-08-22 09:24:21 -0500

    More bugs fixed, commented more code

--- Poker/my_poker_v2.rb
+++ Poker/my_poker_v2.rb
@@ -12,9 +12,9 @@ def introduction # Checks for multiple players and creates the Player objects
       number.times do |index|  
         print "Player #{index + 1} name: "
         name = gets.chomp
-        player = Player.new
+        player = Player.new     # Creation new Player object
         player.data[:name] = name
-        players << player
+        players << player     # Stored into array
         puts "Hi " + players[index].data[:name]
       end
       players
@@ -27,13 +27,13 @@ def introduction # Checks for multiple players and creates the Player objects
 end
 
 def init_round? # Initializes the game 
-  puts "Would you like to play Draw Poker? (yes/no)"
+  puts "Would you like to play Draw Poker? (yes/no)"  
   "yes" == gets.chomp.downcase
 end
 
 def deal(players, deck) #Deals 5 cards to each player
     5.times do 
-      players.each do |player|
+      players.each do |player|      # Deals card to each player before it deals second card
         player.data[:hand] << deck.deal
       end
     end
@@ -59,29 +59,29 @@ def betting(players, pot) # Loop through players as they check/bet/fold/etc
       players.each do |player|
             if player.data[:action] != nil      # Player has already checked or bet
                 if player.data[:bet] < minimum_bet(players) # Player has to check, bet or fold now
-                    puts "#{player.data[:name]}, here are your cards: #{player.data[:hand]}"
+                    puts "#{player.data[:name]}, here are your cards: #{player.data[:hand]}"          # SUMMARY 
                     puts "You have bet #{player.data[:bet]} and you have to at least match #{minimum_bet(players)}" 
                     puts "You have #{player.data[:stack]} chips in your stack."
                     puts
-                    temp_player = Player.new
+                    temp_player = Player.new          # Temp player created so that original bet is not lost
                     temp_player = action(temp_player)
-                    min_bet = minimum_bet(players)
+                    min_bet = minimum_bet(players)  # Minimum bet re-checked
                     
-                    if temp_player.data[:action] == "check" && min_bet <= player.data[:stack]
+                    if temp_player.data[:action] == "check" && (min_bet - player.data[:bet]) <= player.data[:stack] # If player checks and has enough to check
                       difference = min_bet - player.data[:bet]
-                      pot+= difference
+                      pot+= difference                              # Appropriate changes to the pot and player's stack
                       player.data[:stack]-= difference
-                      player.data[:bet] = min_bet
+                      player.data[:bet] = min_bet                   # Bet is reset to the minimum bet
                       puts
                       puts "#{player.data[:name]} checks"              
                       puts
-                    elsif temp_player.data[:bet] <= player.data[:stack] && temp_player.data[:bet] > min_bet
+                    elsif temp_player.data[:bet] + min_bet - player.data[:bet] <= player.data[:stack] && temp_player.data[:bet] > 0 # If raise is less than player stack and more than the minimum bet
                       puts
                       puts "#{player.data[:name]} bets #{temp_player.data[:bet]}"
                       puts
-                      player.data[:stack]-=temp_player.data[:bet]
-                      pot+= temp_player.data[:bet]
-                      player.data[:bet]+=temp_player.data[:bet]
+                      player.data[:stack]-= temp_player.data[:bet] + min_bet - player.data[:bet]   
+                      pot+= temp_player.data[:bet] + min_bet - player.data[:bet]
+                      player.data[:bet] = temp_player.data[:bet] + min_bet                        # Player's new bet correctly re-assigned
                     else 
                       puts
                       puts "#{player.data[:name]} folds."
@@ -90,28 +90,28 @@ def betting(players, pot) # Loop through players as they check/bet/fold/etc
                     end
                 end
         
-            else
+            else                                                                              # First time betting
                 puts "#{player.data[:name]}, here are your cards: #{player.data[:hand]}." 
                 puts "The minimum bet is #{minimum_bet(players)}."
                 puts "You have #{player.data[:stack]} chips in your stack."
                 puts
-                min_bet = minimum_bet(players)
+                min_bet = minimum_bet(players)                                                # Minimum bet starts at 0
                 player = action(player)
                
-                if player.data[:action] == "check" && min_bet <= player.data[:stack]
+                if player.data[:action] == "check" && min_bet <= player.data[:stack]          # If player checks, he bets 0
                   player.data[:bet] = min_bet
                   player.data[:stack]-=min_bet
                   pot+=player.data[:bet]
                   puts
                   puts "#{player.data[:name]} checks"              
                   puts
-                elsif player.data[:bet] <= player.data[:stack] && player.data[:bet] + min_bet > min_bet
+                elsif player.data[:bet] <= player.data[:stack] && player.data[:bet] > 0     
                   puts
                   puts "#{player.data[:name]} bets #{player.data[:bet]}"
                   puts
-                  player.data[:bet]+=min_bet
+                  player.data[:bet]+=min_bet                                                  # Minimum bet and player's bet updates total bet 
                   player.data[:stack]-=player.data[:bet]
-                  pot+= player.data[:bet]
+                  pot+= player.data[:bet]                                                     # Pot increases by appropriate amount
                 else 
                   puts
                   puts "#{player.data[:name]} folds."
@@ -120,7 +120,8 @@ def betting(players, pot) # Loop through players as they check/bet/fold/etc
                 end
             end
             
-            if(players_ready?(players))
+            if(players_ready?(players))                                                       # Checks at the end of each player's turn if everyone is ready
+              #players = next_player_shift(players,player)                                     # Shifts the players so that the next person is now first in the array
               reset_round(players)
               return players, pot
             end
@@ -128,11 +129,16 @@ def betting(players, pot) # Loop through players as they check/bet/fold/etc
    end
 end
 
-def next_player_setting(players)
-  
+def next_player_shift(players, player)
+  while(player != players.last)
+    temp_player = players.last
+    players-= [players.last]
+    players.unshift(temp_player)
+  end
+  players
 end
 
-def reset_round(players) # Resets bets to 0 
+def reset_round(players) # Resets bets to 0  and actions to nil
   players.each do |player|
     player.data[:bet] = 0
     player.data[:action] = nil
@@ -142,25 +148,19 @@ end
 
 def players_ready?(players) # Checks if everyone bet the minimum amount
   
-  if players.count == 1
+  if players.count == 1                   # Checks if everyone but one person folded
     true
   else
     min_bet = minimum_bet(players)
-    temp_array = []
-    temp_array << min_bet
-    player_bets = []
     players.each do |player|
-      if player.data[:action] == nil      # Make sure that everyone has had a chance to act
+      if player.data[:action] == nil || player.data[:bet] != min_bet      # Make sure that everyone has had a chance to act or if they haven't put the minimum bet
         return false
       end
-      player_bets << player.data[:bet]
     end
-  
-    temp_array * players.count == player_bets
   end
 end
 
-def minimum_bet(players) # Minimum bet returned from array of players 
+def minimum_bet(players) # Maximum bet returned from array of players 
   minimum_bet = 0
   players.each do |player|
     if player.data[:bet] != nil && player.data[:bet] > minimum_bet then minimum_bet = player.data[:bet] end
@@ -221,10 +221,10 @@ end
 
 def determine_winner(players, pot) #Checks for multiple winners and displays summary
   players = change_array_to_PokerHand(players)
-  winner = players.max {|a,b| a.data[:hand] <=> b.data[:hand] }
+  winner = players.max {|a,b| a.data[:hand] <=> b.data[:hand] }                             # Compares hands to see which hand is greater
   winners = []
   players.each do |player|
-    if player.data[:hand] == winner.data[:hand] then winners << player end
+    if player.data[:hand] == winner.data[:hand] then winners << player end                  # If the selected hand ties any other hand, pot will be split
   end
   if winners.count == 1
       puts "#{winners[0].data[:name]} has won this round with #{winners[0].data[:hand]}."
@@ -235,7 +235,7 @@ def determine_winner(players, pot) #Checks for multiple winners and displays sum
       puts "The pot is split #{division}-way."
       winners.each do |winner|
          puts "#{winner.data[:name]} takes #{pot/division} with #{winner.data[:hand]}"
-         winner.data[:stack]+=pot/division
+         winner.data[:stack]+=pot/division                                                    # Correct amount of chips given to each winner !! Extra chips? 110 / 3
       end
   end
 end
@@ -269,7 +269,7 @@ if init_round?
             players, deck = deal(first_round_players, deck)
             second_round_players, pot = betting(players, pot)
               
-            if second_round_players.count == 1
+            if second_round_players.count == 1                                          # If only one player remains, winner is declared and round is over
                 puts "#{second_round_players[0].data[:name]} wins #{pot} chips"
                 second_round_players[0].data[:stack]+=pot
             else
@@ -277,7 +277,7 @@ if init_round?
                 players, deck = replacement(second_round_players, deck, pot)
                 last_round_players, pot = betting(players,pot)
         
-                if last_round_players.count == 1
+                if last_round_players.count == 1                                        # If only one player remains, winner is declared and round is over
                     puts "#{last_round_players[0].data[:name]} wins #{pot} chips"
                     last_round_players[0].data[:stack]+=pot
                 else  
@@ -286,11 +286,10 @@ if init_round?
                     determine_winner(last_round_players, pot)
                 end
             end
-            
             first_round_players = round_summary(first_round_players)
             pot = 0
             deck = Deck.new
-            puts
+            puts 
       end
       puts "The game is over"
       puts "#{first_round_players[0].data[:name]} has won!"
