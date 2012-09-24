203ba56dd33a5acf9a1932f2d444ffa852ccfe67
Shaan Sapra <ssapra@uchicago.edu>
2012-09-24 16:24:19 -0500

    setup rspec env

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,4 +4,23 @@ describe Player do
   it "should return true" do 
     true.should == true
   end
+  
+  describe "#save_player_action" do
+    it "should deduct the correct amount from their stack" do
+      pot = double("pot")
+      pot.stub(:total => 0)
+      pot.stub(:total=)
+      pot.stub(:save)
+      round = double("round")
+      round.stub(:pot => pot)
+      player = Player.create
+      player.stub(:round => round)
+      stack = player.stack
+      bet_amount = 20
+      player.save_player_action("bet", bet_amount, 20, 20)
+      player.stack.should == stack - bet_amount
+    end
+    
+  end
+  
 end
