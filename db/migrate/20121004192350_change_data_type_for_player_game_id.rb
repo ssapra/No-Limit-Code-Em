152eb6cef067451b8828072f05da1a4507a43c53
Shaan Sapra <ssapra@uchicago.edu>
2012-10-04 14:24:49 -0500

    changed game_id to integer type

--- db/migrate/20121004192350_change_data_type_for_player_game_id.rb
+++ db/migrate/20121004192350_change_data_type_for_player_game_id.rb
@@ -1,13 +1,13 @@
 class ChangeDataTypeForPlayerGameId < ActiveRecord::Migration
   def up
     change_table :players do |t|
-      t.change :game_id, :string
+      t.change :game_id, :integer
     end
   end
 
   def down
     change_table :players do |t|
-      t.change :game_id, :integer
+      t.change :game_id, :string
     end
   end
 end
