fee17085630ec79e5b7226f5de35a0c23fcd6700
Shaan Sapra <ssapra@uchicago.edu>
2012-10-05 16:50:07 -0500

    organized routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ ServerApp::Application.routes.draw do
   
   get "/requests" => 'requests#display', :as => :display #admin display page
   get "/registration" => 'requests#new_player', :as => :register
-  get "/game_state" => 'requests#state', :as => :states #player asks for state
-  
   post "/status" => 'status#status', :as => :status #used to toggle game states
+  
+  get "/game_state" => 'requests#state', :as => :states #player asks for state
   post "/player" => 'requests#player_turn' #player sends action here
   
 end
