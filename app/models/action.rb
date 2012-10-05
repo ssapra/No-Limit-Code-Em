e4cb48779df551581c6f172283c8222b958c016c
Shaan Sapra <ssapra@uchicago.edu>
2012-10-05 01:15:11 -0500

    bet 0 = check

--- app/models/action.rb
+++ app/models/action.rb
@@ -14,7 +14,9 @@ class Action < ActiveRecord::Base
     round = player.round
     min_bet = round.minimum_bet
     minimum_stack = round.smallest_stack
-    if min_bet == player.bet && bet <= player.stack && bet <= minimum_stack
+    if bet == 0
+      Action.save_player_action(player, "check", 0)
+    elsif min_bet == player.bet && bet <= player.stack && bet <= minimum_stack
       Action.save_player_action(player, "bet", bet)
     elsif min_bet == player.bet && bet <= player.stack && bet > minimum_stack
       Action.record_raw_action(player, action, parameter)
