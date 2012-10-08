5bcecaf62c07d68e392f6b57974da3edd180b27a
Fred Lee <fred@enova.com>
2012-10-08 22:20:20 +0200

    removes commented out code.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,5 @@
 class Status < ActiveRecord::Base
   attr_accessible :game, 
-                  # :play, 
                   :registration,
                   :waiting
-                  # :tournament
 end
