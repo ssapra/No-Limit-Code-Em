4712ccc8de1f78d1769f645df4c766786fe05324
nshah <nshah@efl-nshah.cashnetusa.com>
2012-10-09 19:48:14 -0500

    Modified trigger as I had no idea production ran on postgres

--- db/migrate/20121009210650_create_change_logs.rb
+++ db/migrate/20121009210650_create_change_logs.rb
@@ -8,17 +8,31 @@ class CreateChangeLogs < ActiveRecord::Migration
       t.text    :new_value
     end
     execute <<-SQL
-      CREATE TRIGGER track_changes_hand UPDATE OF hand ON players
+      CREATE OR REPLACE FUNCTION track_hand_changes() RETURNS TRIGGER AS $$
       BEGIN
-        INSERT INTO change_logs(record_id, table_name, column_name, new_value, old_value) VALUES (new.id, 'players', 'hand', new.hand, old.hand);
+      IF (TG_OP = 'UPDATE') THEN
+      INSERT INTO change_logs(record_id, table_name, column_name, new_value, old_value) VALUES (NEW.id, 'players', 'hand', NEW.hand, OLD.hand);
+      END IF;
+      RETURN NEW;
       END;
+      $$ LANGUAGE plpgsql;
+      CREATE TRIGGER track_hand_changes_trigger
+      AFTER UPDATE ON players
+      FOR EACH ROW EXECUTE PROCEDURE track_hand_changes();
     SQL
  
     execute <<-SQL
-      CREATE TRIGGER track_changes_action UPDATE OF action ON players
+      CREATE OR REPLACE FUNCTION track_action_changes() RETURNS TRIGGER AS $$
       BEGIN
-        INSERT INTO change_logs(record_id, table_name, column_name, new_value, old_value) VALUES (new.id, 'players', 'action', new.action, old.action);
+      IF (TG_OP = 'UPDATE') THEN
+      INSERT INTO change_logs(record_id, table_name, column_name, new_value, old_value) VALUES (NEW.id, 'players', 'action', NEW.hand, OLD.hand);
+      END IF;
+      RETURN NEW;
       END;
+      $$ LANGUAGE plpgsql;
+      CREATE TRIGGER track_action_changes_trigger
+      AFTER UPDATE ON players
+      FOR EACH ROW EXECUTE PROCEDURE track_action_changes();
     SQL
   end
 end
