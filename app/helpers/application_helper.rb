538ddf6879c015302e695b793729ad5681bf39a9
brousalis <brousapg@gmail.com>
2012-10-11 00:53:51 -0500

    Final table design. Final chip overlays

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,5 +22,19 @@ module ApplicationHelper
       end
     end
   end
-  
+
+  def stack_display(chips, border=true)
+    colors = {:white=>1, :red=>5, :green=>25, :black=>100, :purple=>500, :yellow=>1000, :gray=>5000}
+
+    chip_types = colors.to_a.sort_by{|c|c.last}.reverse.reduce([]) do |list, (color, value)|
+      list += [color.to_s] * (chips / value)
+      chips = chips % value
+      list
+    end.uniq
+
+    chip_types.map { |color|
+      "<i class='chip chip-#{color} #{"chip-bordered" if border}'></i>".html_safe
+    }.join
+  end
+
 end
